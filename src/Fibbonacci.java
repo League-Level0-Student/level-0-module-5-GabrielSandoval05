2034583420bee48db3f2bee118f5590ffade548a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-12 13:51:56 -0700

    6/12/19

--- src/Fibbonacci.java
+++ src/Fibbonacci.java
@@ -2,11 +2,14 @@
 public class Fibbonacci {
 	public static void main(String[] args) {
 		//0,1,1,2,3,5,8,13,21,34,55,89
-		int first = 0;
-		int second = 1;
-		
-		
-		
-		
+		int first = 5;
+		int second = 8;
+		int third = 0;
+		for(int i = 0; i < 5; i++) {
+		third = first + second;
+		first = second;
+		second = third;
+		System.out.println(second);
+		}
 	}
 }
