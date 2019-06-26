381831cc573f44df1612b957abcdd784e669e9f4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-26 13:06:10 -0700

    6/26/19

--- src/forLoopGauntlet.java
+++ src/forLoopGauntlet.java
@@ -12,5 +12,28 @@ public class forLoopGauntlet {
 				System.out.println(b);
 			}
 		}
+		for(int c = 0;c<100;c++) {
+			if(c%2==0) {
+				
+			}
+			else {
+				System.out.println(c);
+			}
+		}
+		for(int d = 0; d<500; d++) {
+			if(d%2==0) {
+				System.out.println(+d+" is even");
+			}
+			else {
+				System.out.println(+d+" is odd");
+			}
+		}
+		for(int e = 0; e<112; e++) {
+			System.out.println(7*e);
+		}
+		for(int f = 0; f<15; f++) {
+			System.out.println("In" +( 2005+f)+" I was " +f);
+		}
+		
 	}
 }
