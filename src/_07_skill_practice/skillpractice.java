4850bfecea451b7a03830fe226ba04e118dd6aad
351054 <351054@0338S-S-R0B6ZHR.sdcs.local>
2019-10-01 14:24:06 -0700

    Week 3 in Hoover yayyyy

--- src/_07_skill_practice/skillpractice.java
+++ src/_07_skill_practice/skillpractice.java
@@ -9,9 +9,9 @@ public class skillpractice {
 		skillpractice skills = new skillpractice();
 		//skills.skill1();
 		//skills.skill2();
-		skills.skill3();
+		//skills.skill3();
 		//skills.skill4();
-		//skills.skill5();
+		skills.skill5();
 	}
 	void skill1() {
 		String meme = JOptionPane.showInputDialog("How many dimes do you have?");
@@ -39,6 +39,33 @@ public class skillpractice {
 	int hi = new Random().nextInt(11);
 	System.out.println(hi);
 	JOptionPane.showMessageDialog(null,"The difference between the two numbers is "+(random-hi));
+	}
+	void skill4() {
+		String city = JOptionPane.showInputDialog("What city do you live in?");
+		if(city.equalsIgnoreCase("San Diego")) {
+			JOptionPane.showMessageDialog(null, "Congrats for living in America's finest city according to a famous person");
+			}
+		else {
+			JOptionPane.showMessageDialog(null, "Now I know where your life went wrong, go fix it pls");
+		}
+		//String meme = JOptionPane.showInputDialog("how many cars does your family have?");
+		
+		//int cars = Integer.parseInt(meme);
+		int cars = 2;
+		if(cars==0) {
+			JOptionPane.showMessageDialog(null, "Do you use public transportation?");
+		}
+		else if(cars>1) {
+			JOptionPane.showMessageDialog(null, "My family's cars have 8 wheels between them");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "the model is a ford expedition");
+		}
+		
+	}
+	void skill5() {
+		String school = JOptionPane.showInputDialog("What is the name of your school");
+		JOptionPane.showMessageDialog(null, school+" is an amazing school!");
 	}
 	
 }
--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -11,17 +11,23 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+String code = JOptionPane.showInputDialog("How many hours did you spend coding this week? (it better be 100)");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+int hours = Integer.parseInt(code);
+if(hours<=2) {
+	JOptionPane.showMessageDialog(null, "Stop watching Youtube and code it instead pls");
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+if(hours>=3) {
+	
+}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
 
 	private static void playBatmanTheme() {
